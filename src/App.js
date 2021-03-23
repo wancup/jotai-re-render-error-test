281ccd711779d454be55e533a4bb87418f7ddde5
wancup <62416191+wancup@users.noreply.github.com>
2021-03-23 13:51:10 +0900

    add prettier and format code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import {atom, useAtom} from "jotai"
-import {useEffect} from "react"
+import { atom, useAtom } from "jotai";
+import { useEffect } from "react";
 
-const countAtom = atom(0)
+const countAtom = atom(0);
 
 const doubleAtom = atom((get) => {
-  return get(countAtom) * 2
-})
+  return get(countAtom) * 2;
+});
 
 const plusAtom = atom((get) => {
-  const count = get(countAtom)
-  const double =  get(doubleAtom)
-  return count + double
-})
+  const count = get(countAtom);
+  const double = get(doubleAtom);
+  return count + double;
+});
 
 function App() {
-  const [count, setCount] = useAtom(countAtom)
-  const [plus] = useAtom(plusAtom)
-  const [plus2] = useAtom(plusAtom)
+  const [count, setCount] = useAtom(countAtom);
+  const [plus] = useAtom(plusAtom);
+  const [plus2] = useAtom(plusAtom);
 
   useEffect(() => {
-    setCount(count)
-  }, [count])
+    setCount(count);
+  }, [count]);
 
   return (
-      <div>
-        {count},{plus}
-        <button onClick={() => setCount(c => c + 1)}>one up</button>
-      </div>
+    <div>
+      {count},{plus}
+      <button onClick={() => setCount((c) => c + 1)}>one up</button>
+    </div>
   );
 }
 
